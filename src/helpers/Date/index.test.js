55783f8b5527a0e99c6e6fe0b10593f8a18e6d5c
Korotsuuki <diacre.benoit@outlook.fr>
2024-04-25 04:07:50 +0200

    Ajout de tout les fichiers pour la mise en ligne du site

---
+++ src/helpers/Date/index.test.js
@@ -0,0 +1,15 @@
+import { getMonth } from './index';
+
+describe("Date helper", () => {
+    describe("When getMonth is called", () => {
+      it("the function returns 'janvier' for the date 2022-01-01", () => {
+        const date = new Date("2022-01-01");
+        expect(getMonth(date)).toBe("janvier");
+      });
+      it("the function returns 'juillet' for the date 2022-07-08", () => {
+        const date = new Date("2022-07-08");
+        expect(getMonth(date)).toBe("juillet");
+      });
+    });
+  });
+
